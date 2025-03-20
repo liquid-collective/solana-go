8524db61b4a5dda3745a5a0003340aec243bd5a9
javip97 <jperez@alluvial.finance>
2025-03-20 10:49:43 +0100

    fix: wrong IncreaseAdditionalValidatorStake bindings

--- programs/stake-pool/instructions.go
+++ programs/stake-pool/instructions.go
@@ -57,14 +57,15 @@ const (
 	Instruction_SetManager
 	Instruction_SetFee
 	Instruction_SetStaker
-	Instruction_DecreaseAdditionalValidatorStake
-	Instruction_IncreaseAdditionalValidatorStake
-	Instruction_Redelegate
-	Instruction_SetFundingAuthority
 	Instruction_DepositSol
+	Instruction_SetFundingAuthority
 	Instruction_WithdrawSol
 	Instruction_CreateTokenMetadata
 	Instruction_UpdateTokenMetadata
+	Instruction_IncreaseAdditionalValidatorStake
+	Instruction_DecreaseAdditionalValidatorStake
+	Instruction_DecreaseValidatorStakeWithReserve
+	Instruction_Redelegate
 	Instruction_DepositStakeWithSlippage
 	Instruction_WithdrawStakeWithSlippage
 	Instruction_DepositSolWithSlippage
